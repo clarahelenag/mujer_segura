d95969bdcc441be87fae92b7c3fc02a2a8845a72
Clara Guerrero <clarahelenag@gmail.com>
2018-09-04 14:14:25 -0500

    prueba de mierda 2

---
+++ CentroAtencionMS/test/system/centros_test.rb
@@ -0,0 +1,51 @@
+require "application_system_test_case"
+
+class CentrosTest < ApplicationSystemTestCase
+  setup do
+    @centro = centros(:one)
+  end
+
+  test "visiting the index" do
+    visit centros_url
+    assert_selector "h1", text: "Centros"
+  end
+
+  test "creating a Centro" do
+    visit centros_url
+    click_on "New Centro"
+
+    fill_in "Descripcion", with: @centro.descripcion
+    fill_in "Latitud", with: @centro.latitud
+    fill_in "Longitud", with: @centro.longitud
+    fill_in "Nombre", with: @centro.nombre
+    fill_in "Url", with: @centro.url
+    click_on "Create Centro"
+
+    assert_text "Centro was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Centro" do
+    visit centros_url
+    click_on "Edit", match: :first
+
+    fill_in "Descripcion", with: @centro.descripcion
+    fill_in "Latitud", with: @centro.latitud
+    fill_in "Longitud", with: @centro.longitud
+    fill_in "Nombre", with: @centro.nombre
+    fill_in "Url", with: @centro.url
+    click_on "Update Centro"
+
+    assert_text "Centro was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Centro" do
+    visit centros_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Centro was successfully destroyed"
+  end
+end
